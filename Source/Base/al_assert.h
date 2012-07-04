cc12bb1b30b14fc5ed93c19c03d5188adde06990
Dave Keck <dave@getshacked.com>
2012-07-03 16:58:16 -1000

    Made assertion log format prettier

--- Source/Base/al_assert.h
+++ Source/Base/al_assert.h
@@ -4,117 +4,101 @@
 #if (TARGET_OS_MAC && !TARGET_OS_IPHONE)
 
     /* OS X */
-    
     #if (!defined(KERNEL) || !KERNEL)
     
         /* Userspace-only */
-        
         #include <stdlib.h>
         #include <stdio.h>
-        #include <unistd.h>
+        #include <stdint.h>
         #include <stdbool.h>
+        #include <unistd.h>
         #include <time.h>
+        #include <libgen.h>
+        #include <string.h>
         
-        #define AL_ASSERT_MESSAGE_FORMAT "%s Assertion failed (process name: %s, pid: %d, file: %s, function: %s, line: %llu): %s\n"
+        #define AL_ASSERT_MESSAGE_FORMAT "=== Assertion failed ===\n  Time: %s  Process: %s (%jd)\n  File: %s:%ju\n  Function: %s\n  Assertion: %s\n"
         #define AL_ASSERT_OR_ABORT(condition) AL_ASSERT_OR_PERFORM((condition), abort())
-        #define AL_ASSERT_OR_PERFORM(condition, action)                                                                                 \
-        ({                                                                                                                              \
-                                                                                                                                        \
-            bool ___evaluated_condition = false;                                                                                        \
-                                                                                                                                        \
-            ___evaluated_condition = (bool)(condition);                                                                                 \
-                                                                                                                                        \
-            if (!___evaluated_condition)                                                                                                \
-            {                                                                                                                           \
-                                                                                                                                        \
-                time_t ___current_time = 0;                                                                                             \
-                const char *___current_time_string = NULL;                                                                              \
-                int ___argc = 0;                                                                                                        \
-                char **___argv = NULL;                                                                                                  \
-                                                                                                                                        \
-                ___current_time = time(NULL);                                                                                           \
-                ___current_time_string = ctime(&___current_time);                                                                       \
-                                                                                                                                        \
-                if (!___current_time_string)                                                                                            \
-                    ___current_time_string = "(unknown time)";                                                                          \
-                                                                                                                                        \
-                ___argc = al_argc();                                                                                                    \
-                ___argv = al_argv();                                                                                                    \
-                                                                                                                                        \
-                fprintf(stderr, AL_ASSERT_MESSAGE_FORMAT, ___current_time_string, (___argc && ___argv ? ___argv[0] : "(unknown)"),      \
-                    getpid(), __FILE__, __PRETTY_FUNCTION__, (unsigned long long)__LINE__, (#condition));                               \
-                                                                                                                                        \
-                action;                                                                                                                 \
-                                                                                                                                        \
-            }                                                                                                                           \
-                                                                                                                                        \
+        #define AL_ASSERT_OR_PERFORM(condition, action)                                                                    \
+        ({                                                                                                                 \
+            bool ___evaluated_condition = (bool)(condition);                                                               \
+            if (!___evaluated_condition)                                                                                   \
+            {                                                                                                              \
+                time_t ___current_time = time(NULL);                                                                       \
+                const char *___current_time_string = ctime(&___current_time);                                              \
+                if (!___current_time_string)                                                                               \
+                    ___current_time_string = "(unknown)";                                                                  \
+                                                                                                                           \
+                int ___argc = al_argc();                                                                                   \
+                char **___argv = al_argv();                                                                                \
+                char *___process_name = strdup(___argc && ___argv ? basename(___argv[0]) : "(unknown)");                   \
+                char *___file_name = strdup(basename(__FILE__));                                                           \
+                                                                                                                           \
+                fprintf(stderr, AL_ASSERT_MESSAGE_FORMAT, ___current_time_string, ___process_name, (intmax_t)getpid(),     \
+                    ___file_name, (uintmax_t)__LINE__, __PRETTY_FUNCTION__, (#condition));                                 \
+                                                                                                                           \
+                action;                                                                                                    \
+                                                                                                                           \
+                free(___process_name),                                                                                     \
+                ___process_name = NULL;                                                                                    \
+                                                                                                                           \
+                free(___file_name),                                                                                        \
+                ___file_name = NULL;                                                                                       \
+            }                                                                                                              \
         })
 
     #else
 
         /* Kernelspace-only */
-        
         #include <libkern/libkern.h>
+        #include <stdint.h>
         
-        #define AL_ASSERT_MESSAGE_FORMAT "Assertion failed (kernel process, file: %s, function: %s, line: %llu): %s\n"
-        #define AL_ASSERT_OR_PERFORM(condition, action)                                                                           \
-        ({                                                                                                                        \
-                                                                                                                                  \
-            bool ___evaluated_condition = false;                                                                                  \
-                                                                                                                                  \
-            ___evaluated_condition = (bool)(condition);                                                                           \
-                                                                                                                                  \
-            if (!___evaluated_condition)                                                                                          \
-            {                                                                                                                     \
-                                                                                                                                  \
-                printf(AL_ASSERT_MESSAGE_FORMAT, __FILE__, __PRETTY_FUNCTION__, (unsigned long long)__LINE__, (#condition));      \
-                                                                                                                                  \
-                action;                                                                                                           \
-                                                                                                                                  \
-            }                                                                                                                     \
-                                                                                                                                  \
+        #define AL_ASSERT_MESSAGE_FORMAT "=== Assertion failed ===\n  File: %s:%ju\n  Function: %s\n  Assertion: %s\n"
+        #define AL_ASSERT_OR_PERFORM(condition, action)                                                                  \
+        ({                                                                                                               \
+            bool ___evaluated_condition = (bool)(condition);                                                             \
+            if (!___evaluated_condition)                                                                                 \
+            {                                                                                                            \
+                printf(AL_ASSERT_MESSAGE_FORMAT, __FILE__, (uintmax_t)__LINE__, __PRETTY_FUNCTION__, (#condition));      \
+                action;                                                                                                  \
+            }                                                                                                            \
         })
 
     #endif
 
 #elif (TARGET_OS_MAC && TARGET_OS_IPHONE)
 
-    /* Userspace iOS */
-    
+    /* iOS */
     #include <stdlib.h>
     #include <stdio.h>
-    #include <unistd.h>
+    #include <stdint.h>
     #include <stdbool.h>
+    #include <unistd.h>
     #include <time.h>
+    #include <libgen.h>
+    #include <string.h>
     
-    #define AL_ASSERT_MESSAGE_FORMAT "%s Assertion failed (pid: %d, file: %s, function: %s, line: %llu): %s\n"
+    #define AL_ASSERT_MESSAGE_FORMAT "=== Assertion failed ===\n  Time: %s  PID: %jd\n  File: %s:%ju\n  Function: %s\n  Assertion: %s\n"
     #define AL_ASSERT_OR_ABORT(condition) AL_ASSERT_OR_PERFORM((condition), abort())
-    #define AL_ASSERT_OR_PERFORM(condition, action)                                                    \
-    ({                                                                                                 \
-                                                                                                       \
-        bool ___evaluated_condition = false;                                                           \
-                                                                                                       \
-        ___evaluated_condition = (bool)(condition);                                                    \
-                                                                                                       \
-        if (!___evaluated_condition)                                                                   \
-        {                                                                                              \
-                                                                                                       \
-            time_t ___current_time = 0;                                                                \
-            const char *___current_time_string = NULL;                                                 \
-                                                                                                       \
-            ___current_time = time(NULL);                                                              \
-            ___current_time_string = ctime(&___current_time);                                          \
-                                                                                                       \
-            if (!___current_time_string)                                                               \
-                ___current_time_string = "(unknown time)";                                             \
-                                                                                                       \
-            fprintf(stderr, AL_ASSERT_MESSAGE_FORMAT, ___current_time_string, getpid(), __FILE__,      \
-                __PRETTY_FUNCTION__, (unsigned long long)__LINE__, (#condition));                      \
-                                                                                                       \
-            action;                                                                                    \
-                                                                                                       \
-        }                                                                                              \
-                                                                                                       \
+    #define AL_ASSERT_OR_PERFORM(condition, action)                                                   \
+    ({                                                                                                \
+        bool ___evaluated_condition = (bool)(condition);                                              \
+        if (!___evaluated_condition)                                                                  \
+        {                                                                                             \
+            time_t ___current_time = time(NULL);                                                      \
+            const char *___current_time_string = ctime(&___current_time);                             \
+            if (!___current_time_string)                                                              \
+                ___current_time_string = "(unknown)";                                                 \
+                                                                                                      \
+            char *___file_name = strdup(basename(__FILE__));                                          \
+                                                                                                      \
+            fprintf(stderr, AL_ASSERT_MESSAGE_FORMAT, ___current_time_string, (intmax_t)getpid(),     \
+                ___file_name, (uintmax_t)__LINE__, __PRETTY_FUNCTION__, (#condition));                \
+                                                                                                      \
+            action;                                                                                   \
+                                                                                                      \
+            free(___file_name),                                                                       \
+            ___file_name = NULL;                                                                      \
+        }                                                                                             \
     })
 
 #endif
